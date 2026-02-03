d4bd598b85c0bec003e8e0f3550c8757e75f5a02
Maxi Barmetler <maxi.barmetler@gmail.com>
2026-02-03 20:57:18 +0100

    d10p1

--- d10/d10.rs
+++ d10/d10.rs
@@ -1,16 +1,124 @@
-use aoc2016::grid::Grid;
+use aoc2016::grid::{Grid, Pos};
+use std::collections::HashSet;
+
+fn reachable_peaks(
+    grid: &Grid<u8>,
+    cache: &mut Grid<Option<HashSet<Pos>>>,
+    p @ [x, y]: Pos,
+) -> usize {
+    if let Some(ref c) = cache[p] {
+        return c.len();
+    }
+    let v = grid[p];
+    if !(0..=9).contains(&v) {
+        cache[p] = Some(HashSet::new());
+        return 0;
+    }
+    if v == 9 {
+        cache[p] = Some(HashSet::from([p]));
+        return 1;
+    }
+    let mut new_cache = HashSet::new();
+    for n in [[x + 1, y], [x, y + 1], [x - 1, y], [x, y - 1]] {
+        if grid.is_inside(n) && grid[n] == v + 1 {
+            reachable_peaks(grid, cache, n);
+            if let Some(ref c) = cache[n] {
+                new_cache.extend(c);
+            }
+        }
+    }
+    let l = new_cache.len();
+    cache[p] = Some(new_cache);
+    l
+}
+
+fn trailheads(grid: &Grid<u8>) -> (usize, Grid<usize>) {
+    let mut total = 0;
+    let mut cache = Grid::new(grid.size);
+    for i in 0..grid.len() {
+        if grid[i] == 0 {
+            total += reachable_peaks(
+                grid,
+                &mut cache,
+                [(i % grid.width()) as isize, (i / grid.height()) as isize],
+            );
+        }
+    }
+    (total, cache.map(|c| c.map(|c| c.len()).unwrap_or_default()))
+}
 
 fn main() {
-    let input = r#"
-        89010123
-        78121874
-        87430965
-        96549874
-        45678903
-        32019012
-        01329801
-        10456732
-        "#;
+    let input = include_str!("input.txt");
     let grid = Grid::from_lines(input.lines(), |_, c| c as u8 - b'0');
-    println!("{}", grid);
+    let (total, _) = trailheads(&grid);
+    println!("Part1: {}", total);
+}
+
+#[cfg(test)]
+pub mod tests {
+    use super::*;
+
+    #[test]
+    fn test_trailheads_1() {
+        let grid = Grid::from_lines(
+            r#"
+            0123
+            1234
+            8765
+            9876
+            "#
+            .lines(),
+            |_, c| c as u8 - b'0',
+        );
+        let (total, th) = trailheads(&grid);
+        assert_eq!(total, 1);
+        assert_eq!(th.to_string(), "1111\n1111\n1111\n1111");
+    }
+
+    #[test]
+    fn test_trailheads_2() {
+        let grid = Grid::from_lines(
+            r#"
+            ...0...
+            ...1...
+            ...2...
+            6543456
+            7.....7
+            8.....8
+            9.....9
+            "#
+            .lines(),
+            |_, c| if c == '.' { 10 } else { c as u8 - b'0' },
+        );
+        let (total, th) = trailheads(&grid);
+        assert_eq!(total, 2);
+        assert_eq!(
+            th.to_string(),
+            "0002000\n0002000\n0002000\n1112111\n1000001\n1000001\n1000001",
+        );
+    }
+
+    #[test]
+    fn test_trailheads_3() {
+        let grid = Grid::from_lines(
+            r#"
+            89010123
+            78121874
+            87430965
+            96549874
+            45678903
+            32019012
+            01329801
+            10456732
+            "#
+            .lines(),
+            |_, c| c as u8 - b'0',
+        );
+        let (total, th) = trailheads(&grid);
+        assert_eq!(total, 36);
+        assert_eq!(
+            th.to_string(),
+            "01556333\n01555113\n12555133\n12551333\n55333133\n55111333\n55111133\n05111100",
+        );
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@
 pub mod grid {
     use std::fmt::{Display, Formatter};
     use std::iter::once;
-    use std::ops::{Index, IndexMut};
+    use std::ops::{Deref, Index, IndexMut};
 
     pub type Pos = [isize; 2];
     pub type Size = [usize; 2];
@@ -22,7 +22,35 @@ pub mod grid {
         pub size: Size,
     }
 
+    impl<Cell: Default> Grid<Cell> {
+        pub fn new(size: Size) -> Self {
+            let c = size[0] * size[1];
+            let mut cells = Vec::with_capacity(c);
+            cells.resize_with(c, Cell::default);
+            Self { cells, size }
+        }
+    }
+
     impl<Cell> Grid<Cell> {
+        pub fn new_with(size: Size, f: impl FnMut() -> Cell) -> Self {
+            let c = size[0] * size[1];
+            let mut cells = Vec::with_capacity(c);
+            cells.resize_with(c, f);
+            Self { cells, size }
+        }
+
+        pub fn new_with_pos(size: Size, mut f: impl FnMut(Pos) -> Cell) -> Self {
+            let c = size[0] * size[1];
+            let mut cells = Vec::with_capacity(c);
+            let mut i = 0;
+            cells.resize_with(c, || {
+                let r = f([(i % size[0]) as isize, (i / size[1]) as isize]);
+                i += 1;
+                r
+            });
+            Self { cells, size }
+        }
+
         pub fn from_lines<'s>(
             lines: impl IntoIterator<Item = &'s str>,
             mut create_cell: impl FnMut(Pos, char) -> Cell,
@@ -49,6 +77,30 @@ pub mod grid {
         }
     }
 
+    impl<Cell, const W: usize, const H: usize> From<[[Cell; W]; H]> for Grid<Cell> {
+        fn from(value: [[Cell; W]; H]) -> Self {
+            Self {
+                cells: value.into_iter().flatten().collect(),
+                size: [W, H],
+            }
+        }
+    }
+
+    impl<Cell> Deref for Grid<Cell> {
+        type Target = [Cell];
+
+        fn deref(&self) -> &Self::Target {
+            &self.cells
+        }
+    }
+
+    impl<Cell> Grid<Cell> {
+        pub fn rows(&self) -> impl Iterator<Item = &[Cell]> {
+            let [width, height] = self.size;
+            (0..height).map(move |y| &self.cells[y * width..(y + 1) * width])
+        }
+    }
+
     impl<Cell> Grid<Cell> {
         pub fn is_inside(&self, [x, y]: Pos) -> bool {
             (0..self.size[0] as isize).contains(&x) && (0..self.size[1] as isize).contains(&y)
@@ -61,26 +113,55 @@ pub mod grid {
         pub fn height(&self) -> usize {
             self.size[1]
         }
+
+        pub fn len(&self) -> usize {
+            self.size[0] * self.size[1]
+        }
+
+        pub fn is_empty(&self) -> bool {
+            self.size[0] == 0 || self.size[1] == 0
+        }
     }
 
     impl<Cell> Index<[isize; 2]> for Grid<Cell> {
         type Output = Cell;
 
         fn index(&self, index: [isize; 2]) -> &Self::Output {
-            let i = idx(index, self.size);
-            &self.cells[i]
+            let index = idx(index, self.size);
+            &self.cells[index]
         }
     }
 
     impl<Cell> IndexMut<[isize; 2]> for Grid<Cell> {
         fn index_mut(&mut self, index: [isize; 2]) -> &mut Self::Output {
-            let i = idx(index, self.size);
-            &mut self.cells[i]
+            let index = idx(index, self.size);
+            &mut self.cells[index]
+        }
+    }
+
+    impl<Cell> Index<usize> for Grid<Cell> {
+        type Output = Cell;
+
+        fn index(&self, index: usize) -> &Self::Output {
+            &self.cells[index]
+        }
+    }
+
+    impl<Cell> IndexMut<usize> for Grid<Cell> {
+        fn index_mut(&mut self, index: usize) -> &mut Self::Output {
+            &mut self.cells[index]
         }
     }
 
     impl<Cell> Grid<Cell> {
-        pub fn map<NewCell>(self, map_fn: impl Fn(Pos, Cell) -> NewCell) -> Grid<NewCell> {
+        pub fn map<NewCell>(self, map_fn: impl Fn(Cell) -> NewCell) -> Grid<NewCell> {
+            Grid {
+                cells: self.cells.into_iter().map(map_fn).collect(),
+                size: self.size,
+            }
+        }
+
+        pub fn map_pos<NewCell>(self, map_fn: impl Fn(Pos, Cell) -> NewCell) -> Grid<NewCell> {
             let width = self.width();
             Grid {
                 cells: self
