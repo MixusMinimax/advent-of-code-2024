09a454011d5082db03826d0e04c996186592fe10
Maxi Barmetler <maxi.barmetler@gmail.com>
2026-02-03 18:05:16 +0100

    d10 input

--- d10/d10.rs
+++ d10/d10.rs
@@ -1,1 +1,16 @@
-fn main() {}
+use aoc2016::grid::Grid;
+
+fn main() {
+    let input = r#"
+        89010123
+        78121874
+        87430965
+        96549874
+        45678903
+        32019012
+        01329801
+        10456732
+        "#;
+    let grid = Grid::from_lines(input.lines(), |_, c| c as u8 - b'0');
+    println!("{}", grid);
+}
